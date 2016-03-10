2f5ecebecb6eeb8a06692552be69869c993d0c57
Myles Braithwaite <me@mylesbraithwaite.com>
2016-03-10 09:56:35 -0500

    Got backup working.

--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 
 import os
+import datetime
 import ConfigParser
 
-from fabric.api import env, task, roles, put, cd, hosts, local
+from fabric.api import env, task, roles, put, cd, hosts, local, run, get
 
 from boto.s3.key import Key
 from boto.s3.connection import S3Connection
 
-env.user = 'myles_mylbe'
-env.hosts = ['ssh.phx.nearlyfreespeech.net']
+env.user = 'myles'
 env.use_ssh_config = True
+env.cwd = '/srv/www/myl.be/www/'
+env.hosts = ['bear.mylesbraithwaite.com']
 
 env.roledefs = {
-    'yourls': ['ssh.phx.nearlyfreespeech.net']
+    'yourls': ['bear.mylesbraithwaite.com']
 }
+
+env.database_name = 'myl_be_www'
+env.database_username = 'myl_be_www'
+env.database_password = 'myl_be_www'
 
 
 @task
 @roles('yourls')
 def deploy_www():
-    with cd('/home/public/'):
+    with cd('/srv/www/myl.be/www/html/'):
         put('./www/index.php', 'index.php')
         put('./www/json.php', 'json.php')
         put('./www/rss.php', 'rss.php')
@@ -55,3 +61,40 @@ def update_landing_page():
                                                  'CustomLandingPage.html'))
 
     local('cp ./files/landing-page.html "{0}"'.format(dropshare_landing_page))
+
+
+@task
+@roles('yourls')
+def backup_mysql():
+    timestamp = datetime.datetime.now().isoformat().replace(':', '-')
+
+    file_name = '/tmp/%(database)s-backup-%(timestamp)s.sql.gz' % {
+        'database': env.database_name,
+        'timestamp': timestamp
+    }
+
+    run('mysqldump -u %(username)s -p%(password)s %(database)s | '
+        'gzip > %(file_name)s' % {'username': env.database_username,
+                                  'password': env.database_password,
+                                  'database': env.database_name,
+                                  'file_name': file_name})
+
+    get(file_name, os.path.join('./backups/', os.path.basename(file_name)))
+
+    run('rm "%s"' % file_name)
+
+
+@task
+@roles('yourls')
+def backup_config():
+    timestamp = datetime.datetime.now().isoformat().replace(':', '-')
+
+    get('html/user/config.php',
+        './backups/config-backup-%(timestamp)s.php' % {'timestamp': timestamp})
+
+
+@task
+@roles('yourls')
+def backup():
+    backup_mysql()
+    backup_config()
